7d53ef07ae838a707e2ca8a467e51cd1174cf388
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2014-04-16 12:28:54 +0200

    or1k: change stack variable naming in libgloss
    Before a pointer to the top of the stack was required by sbrk to
    determine whether sufficient space is available. Instead we now use
    three variables in crt0.S:
    
     * stack_top: points to top of the stack
    
     * stack_bottom: points to bottom of the reserved stack
    
     * stack_size: The size of the stack, set by the define STACK_SIZE
    
    This has the advantage, that we can simply compare to the stack_bottom
    can later easily allocate multiple stacks for the different cores and
    just point to top and bottom of the reserved area.

--- libgloss/or1k/sbrk.c
+++ libgloss/or1k/sbrk.c
@@ -35,9 +35,6 @@
 #undef errno
 extern int  errno;
 
-#define STACK_BUFFER  65536	/*!< Reserved stack space in bytes. */
-
-
 /* -------------------------------------------------------------------------- */
 /*!Extend the heap
 
@@ -69,12 +66,12 @@ _sbrk (int nbytes)
   extern int  end;		/* start of free memory (as symbol) */
 
   /* Value set by crt0.S */
-  extern void *stack;		/* end of free memory */
+  extern void *stack_bottom; /* end of free memory */
 
   /* The statically held previous end of the heap, with its initialization. */
   static void *heap_ptr = (void *)&end;		/* Previous end */
 
-  if ((stack - (heap_ptr + nbytes)) > STACK_BUFFER )
+  if ((stack_bottom - (heap_ptr + nbytes)) > 0 )
     {
       void *base  = heap_ptr;
       heap_ptr   += nbytes;
